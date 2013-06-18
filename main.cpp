24efcce03d1698679adb587cba018a9166e217f1
Enel <spiritring@gmail.com>
2013-06-18 16:45:44 +0800

    词法分析器
    词法分析器

--- main.cpp
+++ main.cpp
@@ -3,9 +3,13 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <set>
+#include <iostream>
+
 typedef std::string TSString;
 typedef std::wstring TSWString;
-typedef std::map TSMap;
+#define TSMap std::map
+#define TSSet std::set
 
 // 定义TS实例类型
 enum EN_TS_Type{
@@ -19,6 +23,7 @@ enum EN_TS_Type{
     TS_wstring,         //宽字符UTF-16
     TS_class,           //类
     TS_interface,       //接口
+    TS_Function,        //函数
     TS_Type_Max
 };
 
@@ -32,25 +37,59 @@ enum EN_TS_ClassRule{
 };
 
 // 类型基类
-class TSType{
+class TSObject{
 public:
     EN_TS_Type m_iType;
     TSString m_sName;
 };
 
-class TSBaseType : public TSType {
+class TSBaseObject : public TSObject {
 public:
-
+    TSString m_Value;
 };
 
-class TSClassType : public TSType{
+class TSClassObject : public TSObject{
 public:
-    TSMap<EN_TS_ClassRule, TSMap<EN_TS_Type, TSString>> m_Info;
+    TSMap<EN_TS_ClassRule, TSMap<EN_TS_Type, TSSet<TSObject*>>> m_Info;
 };
 
-int main(int argc, char *argv[])
-{
+class TSEngine {
+public:
+    void RunLine(TSString& str) {
+        std::cout << str << std::endl;
+
+
+
+    }
+
+    void RunFile(TSString& str){
+        int i = 0;
+        char c = str[i];
+        TSString line;
+        int begin = 0;
+        while((int)str.size() > i) {
+            c = str[i];
+            i ++;
+            if(begin == 0 && (c == ' ' || c == '\11' || c == '\n')){
+                continue;
+            }
+            begin = 1;
+            line += c;
+            if (c == ';') {
+                RunLine(line);
+                line = "";
+                begin = 0;
+            }
+        }
+    }
+};
+
+int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
-    
+    TSString oneLine = "            int a = 50; a=60;";
+
+    TSEngine tse;
+    tse.RunFile(oneLine);
+
     return a.exec();
 }
